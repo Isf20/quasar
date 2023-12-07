54f21f3d2b48f3d33a3eda5cb87441748550967d
Isf20 <Ilada.isf@gmail.com>
2023-12-07 16:08:13 +0700

    Ilada Saengpirom 6404101379

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -9,8 +9,8 @@
       <q-input
         filled
         v-model="name"
-        label="Your name *"
-        hint="Name and surname"
+        label="Your name1 *"
+        hint="Name and surname1"
         lazy-rules
         :rules="[ val => val && val.length > 0 || 'Please type something']"
       />
